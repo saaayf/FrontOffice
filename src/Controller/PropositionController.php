245d5944e1a8e8670dd8cc2fb85bb99b373cb777
sayf eddine rguez <101209605+saaayf@users.noreply.github.com>
2024-02-19 22:29:29 +0100

    Front+back crud completed

--- src/Controller/PropositionController.php
+++ src/Controller/PropositionController.php
@@ -23,6 +23,14 @@ class PropositionController extends AbstractController
             "id_projet" => $id_projet
         ]);
     }
+      #[Route('/back', name: 'app_proposition_index_back', methods: ['GET'])]
+        public function indexBack(PropositionRepository $propositionRepository): Response
+        {
+            return $this->render('proposition/indexBack.html.twig', [
+                'propositions' => $propositionRepository->findAll()
+
+            ]);
+        }
 
     #[Route('/new/{id_projet}', name: 'app_proposition_new', methods: ['GET', 'POST'])]
     public function new($id_projet,Request $request, EntityManagerInterface $entityManager): Response
@@ -72,15 +80,43 @@ class PropositionController extends AbstractController
             'projet_id' => $proposition->getIdProjet()->getId()
         ]);
     }
+     #[Route('/{id}/editBack', name: 'app_proposition_edit_back', methods: ['GET', 'POST'])]
+        public function editBack(Request $request, Proposition $proposition, EntityManagerInterface $entityManager): Response
+        {
+            $form = $this->createForm(PropositionType::class, $proposition);
+            $form->handleRequest($request);
 
-    #[Route('/{id}', name: 'app_proposition_delete', methods: ['POST'])]
-    public function delete(Request $request, Proposition $proposition, EntityManagerInterface $entityManager): Response
+            if ($form->isSubmitted() && $form->isValid()) {
+                $entityManager->flush();
+
+                return $this->redirectToRoute('app_proposition_index_back', [], Response::HTTP_SEE_OTHER);
+            }
+
+            return $this->renderForm('proposition/editBack.html.twig', [
+                'proposition' => $proposition,
+                'form' => $form,
+                'projet_id' => $proposition->getIdProjet()->getId()
+            ]);
+        }
+
+    #[Route('/{id}/{id_projet}', name: 'app_proposition_delete', methods: ['POST'])]
+    public function delete($id_projet,Request $request, Proposition $proposition, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$proposition->getId(), $request->request->get('_token'))) {
             $entityManager->remove($proposition);
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('app_proposition_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_proposition_index', ['id_projet' => $id_projet], Response::HTTP_SEE_OTHER);
     }
+      #[Route('Deleteback/{id}', name: 'app_proposition_delete_back', methods: ['POST'])]
+        public function deleteBack(Request $request, Proposition $proposition, EntityManagerInterface $entityManager): Response
+        {
+            if ($this->isCsrfTokenValid('delete'.$proposition->getId(), $request->request->get('_token'))) {
+                $entityManager->remove($proposition);
+                $entityManager->flush();
+            }
+
+            return $this->redirectToRoute('app_proposition_index_back', [], Response::HTTP_SEE_OTHER);
+        }
 }
